4dc7fb62223c724286c8f3a74e7761ecef834b75
hotrice37 <shubinpokhrel4@gmail.com>
2023-05-12 19:19:33 +0545

    navbar css

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,8 +1,8 @@
-import "./topbar.css";
-import { Search, Person, Chat, Notifications } from "@material-ui/icons";
-import { Link } from "react-router-dom";
-import { useContext } from "react";
-import { AuthContext } from "../../context/AuthContext";
+import './topbar.css';
+import { Search, Person, Chat, Notifications } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
 
 export default function Topbar() {
   const { user } = useContext(AuthContext);
@@ -10,8 +10,8 @@ export default function Topbar() {
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
-        <Link to="/" style={{ textDecoration: "none" }}>
-          <span className="logo">Lamasocial</span>
+        <Link to="/" style={{ textDecoration: 'none' }}>
+          <span className="topbartitle">SANCHAR</span>
         </Link>
       </div>
       <div className="topbarCenter">
@@ -25,8 +25,8 @@ export default function Topbar() {
       </div>
       <div className="topbarRight">
         <div className="topbarLinks">
-          <span className="topbarLink">Homepage</span>
-          <span className="topbarLink">Timeline</span>
+          <span className="topbarLink">HOMEPAGE</span>
+          <span className="topbarLink">TIMELINE</span>
         </div>
         <div className="topbarIcons">
           <div className="topbarIconItem">
@@ -47,7 +47,7 @@ export default function Topbar() {
             src={
               user.profilePicture
                 ? PF + user.profilePicture
-                : PF + "person/noAvatar.png"
+                : PF + 'person/noAvatar.png'
             }
             alt=""
             className="topbarImg"
