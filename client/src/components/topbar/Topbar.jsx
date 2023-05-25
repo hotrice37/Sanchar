80a7de4606631e08c4549020439b57352e0a95e7
hotrice37 <shubinpokhrel4@gmail.com>
2023-05-25 07:34:32 +0545

    search messenger and user

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,12 +1,42 @@
 import './topbar.css';
 import { Search, Person, Chat, Notifications } from '@material-ui/icons';
 import { Link } from 'react-router-dom';
-import { useContext } from 'react';
+import { useContext, useState, useEffect, useRef } from 'react';
 import { AuthContext } from '../../context/AuthContext';
+import axios from 'axios';
 
 export default function Topbar() {
   const { user } = useContext(AuthContext);
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const [searchInput, setSearchInput] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const searchInputRef = useRef(null);
+
+  useEffect(() => {
+    const fetchSearchResults = async () => {
+      try {
+        const response = await axios.get(
+          '/users/search?username=' + searchInput
+        );
+        setSearchResults(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (searchInput.trim() !== '') {
+      fetchSearchResults();
+    } else {
+      setSearchResults([]);
+    }
+  }, [searchInput]);
+
+  const handleSearchBarClick = () => {
+    setSearchInput('');
+    setSearchResults([]);
+    searchInputRef.current.focus();
+  };
+
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
@@ -15,13 +45,27 @@ export default function Topbar() {
         </Link>
       </div>
       <div className="topbarCenter">
-        <div className="searchbar">
+        <div className="searchbar" onClick={handleSearchBarClick}>
           <Search className="searchIcon" />
           <input
+            ref={searchInputRef}
             placeholder="Search for friend, post or video"
             className="searchInput"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
           />
         </div>
+        {searchResults.length > 0 && (
+          <ul className="searchResults">
+            {searchResults.map((result) => (
+              <li key={result._id}>
+                <Link to={`/profile/${result.username}`}>
+                  {result.username}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
       <div className="topbarRight">
         <div className="topbarLinks">
@@ -34,8 +78,10 @@ export default function Topbar() {
             <span className="topbarIconBadge">1</span>
           </div>
           <div className="topbarIconItem">
-            <Chat />
-            <span className="topbarIconBadge">2</span>
+            <Link to="/messenger">
+              <Chat className="icon" />
+              <span className="topbarIconBadge">2</span>
+            </Link>
           </div>
           <div className="topbarIconItem">
             <Notifications />
@@ -49,7 +95,7 @@ export default function Topbar() {
                 ? PF + user.profilePicture
                 : PF + 'person/noAvatar.png'
             }
-            alt=""
+            alt="profile"
             className="topbarImg"
           />
         </Link>
--- client/src/pages/messenger/Messenger.jsx
+++ client/src/pages/messenger/Messenger.jsx
@@ -1,76 +1,118 @@
-import "./messenger.css";
-import Topbar from "../../components/topbar/Topbar";
-import Conversation from "../../components/conversations/Conversation";
-import Message from "../../components/message/Message";
-import ChatOnline from "../../components/chatOnline/ChatOnline";
-import { useContext, useEffect, useRef, useState } from "react";
-import { AuthContext } from "../../context/AuthContext";
-import axios from "axios";
-import { io } from "socket.io-client";
+import './messenger.css';
+import Topbar from '../../components/topbar/Topbar';
+import Conversation from '../../components/conversations/Conversation';
+import Message from '../../components/message/Message';
+import ChatOnline from '../../components/chatOnline/ChatOnline';
+import { useContext, useEffect, useRef, useState } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+import axios from 'axios';
+import { io } from 'socket.io-client';
 
 export default function Messenger() {
   const [conversations, setConversations] = useState([]);
   const [currentChat, setCurrentChat] = useState(null);
   const [messages, setMessages] = useState([]);
-  const [newMessage, setNewMessage] = useState("");
-  const [arrivalMessage, setArrivalMessage] = useState(null);
+  const [newMessage, setNewMessage] = useState('');
   const [onlineUsers, setOnlineUsers] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null); // New state variable
+
   const socket = useRef();
   const { user } = useContext(AuthContext);
   const scrollRef = useRef();
 
   useEffect(() => {
-    socket.current = io("ws://localhost:8900");
-    socket.current.on("getMessage", (data) => {
-      setArrivalMessage({
-        sender: data.senderId,
-        text: data.text,
-        createdAt: Date.now(),
-      });
+    socket.current = io('ws://localhost:8900');
+
+    socket.current.on('getMessage', (data) => {
+      setMessages((prevMessages) => [...prevMessages, data]);
     });
+
+    return () => {
+      socket.current.disconnect();
+    };
   }, []);
 
   useEffect(() => {
-    arrivalMessage &&
-      currentChat?.members.includes(arrivalMessage.sender) &&
-      setMessages((prev) => [...prev, arrivalMessage]);
-  }, [arrivalMessage, currentChat]);
+    socket.current.emit('addUser', user._id);
 
-  useEffect(() => {
-    socket.current.emit("addUser", user._id);
-    socket.current.on("getUsers", (users) => {
+    socket.current.on('getUsers', (users) => {
       setOnlineUsers(
-        user.followings.filter((f) => users.some((u) => u.userId === f))
+        user.followings.filter((following) =>
+          users.some((user) => user.userId === following)
+        )
       );
     });
+
+    return () => {
+      socket.current.emit('userDisconnect');
+      socket.current.off();
+    };
   }, [user]);
 
   useEffect(() => {
     const getConversations = async () => {
       try {
-        const res = await axios.get("/conversations/" + user._id);
+        const res = await axios.get('/conversations/' + user._id);
         setConversations(res.data);
       } catch (err) {
         console.log(err);
       }
     };
+
     getConversations();
-  }, [user._id]);
+  }, [user._id, selectedUser]);
 
   useEffect(() => {
-    const getMessages = async () => {
-      try {
-        const res = await axios.get("/messages/" + currentChat?._id);
-        setMessages(res.data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    getMessages();
+    if (currentChat) {
+      const getMessages = async () => {
+        try {
+          const res = await axios.get('/messages/' + currentChat._id);
+          setMessages(res.data);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+
+      getMessages();
+    }
   }, [currentChat]);
+
+  const handleSearch = async () => {
+    try {
+      const res = await axios.get('/users/search?username=' + searchQuery);
+      setSearchResults(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const startConversation = async (userId) => {
+    try {
+      const res = await axios.get(`/conversations/find/${user._id}/${userId}`);
+      if (res.data) {
+        setCurrentChat(res.data);
+      } else {
+        const newConversation = await axios.post('/conversations', {
+          senderId: user._id,
+          receiverId: userId,
+        });
+        setCurrentChat(newConversation.data);
+      }
+
+      setSelectedUser(userId);
+
+      setSearchQuery('');
+      setSearchResults([]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     const message = {
       sender: user._id,
       text: newMessage,
@@ -81,46 +123,62 @@ export default function Messenger() {
       (member) => member !== user._id
     );
 
-    socket.current.emit("sendMessage", {
+    socket.current.emit('sendMessage', {
       senderId: user._id,
       receiverId,
       text: newMessage,
     });
 
     try {
-      const res = await axios.post("/messages", message);
+      const res = await axios.post('/messages', message);
       setMessages([...messages, res.data]);
-      setNewMessage("");
+      setNewMessage('');
     } catch (err) {
       console.log(err);
     }
   };
 
   useEffect(() => {
-    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+    scrollRef.current?.scrollIntoView({ behavior: 'smooth' });
   }, [messages]);
 
   return (
     <>
       <Topbar />
       <div className="messenger">
-        <div className="chatMenu">
-          <div className="chatMenuWrapper">
-            <input placeholder="Search for friends" className="chatMenuInput" />
-            {conversations.map((c) => (
-              <div onClick={() => setCurrentChat(c)}>
-                <Conversation conversation={c} currentUser={user} />
-              </div>
-            ))}
-          </div>
+        <div className="chatMenuWrapper">
+          <input
+            placeholder="Search for friends"
+            className="chatMenuInput"
+            value={searchQuery}
+            onChange={(e) => {
+              setSearchQuery(e.target.value);
+              handleSearch();
+            }}
+          />
+
+          {searchResults.map((user) => (
+            <div key={user._id} onClick={() => startConversation(user._id)}>
+              {user.username}
+            </div>
+          ))}
+          {conversations.map((c) => (
+            <div key={c._id} onClick={() => setCurrentChat(c)}>
+              <Conversation conversation={c} currentUser={user} />
+            </div>
+          ))}
         </div>
+
         <div className="chatBox">
           <div className="chatBoxWrapper">
             {currentChat ? (
               <>
                 <div className="chatBoxTop">
-                  {messages.map((m) => (
-                    <div ref={scrollRef}>
+                  {messages.map((m, index) => (
+                    <div
+                      ref={index === messages.length - 1 ? scrollRef : null}
+                      key={m._id}
+                    >
                       <Message message={m} own={m.sender === user._id} />
                     </div>
                   ))}
